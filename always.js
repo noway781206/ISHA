b57339e106a952e51ab987a6026694cc9b4945f8
Ming <noway781206@gmail.com>
2022-03-03 04:01:53 +0800

    0303_rwdMenu

--- always.js
+++ always.js
@@ -1,13 +1,23 @@
 {
-      fetch("image/social-color-1_logo-facebook.svg")
-  .then(response => response.text())
-  .then(svg => document.querySelector('#facebook_Icon').insertAdjacentHTML("afterbegin", svg));
+  //     fetch("image/social-color-1_logo-facebook.svg")
+  // .then(response => response.text())
+  // .then(svg => document.querySelector('#facebook_Icon').insertAdjacentHTML("afterbegin", svg));
 
-  fetch("image/social-color-1_logo-youtube.svg")
-  .then(response => response.text())
-  .then(svg => document.querySelector('#youtube_Icon').insertAdjacentHTML("afterbegin", svg)); 
+  axios.get('image/social-color-1_logo-facebook.svg')
+.then((res) => {
+    this.markDownData = res.data
+    document.querySelector('#facebook_Icon').insertAdjacentHTML("afterbegin", res.data)
+})
+  // fetch("image/social-color-1_logo-youtube.svg")
+  // .then(response => response.text())
+  // .then(svg => document.querySelector('#youtube_Icon').insertAdjacentHTML("afterbegin", svg)); 
+  axios.get('image/social-color-1_logo-youtube.svg')
+  .then((res) => {
+      this.markDownData = res.data
+      document.querySelector('#youtube_Icon').insertAdjacentHTML("afterbegin", res.data)
+  })
 
-
+  console.log(2,document.querySelector('.menu-button'));
 
   function menuOffset(){
     
@@ -54,7 +64,50 @@
   
   let menu = document.querySelector('#menu');
   let topBar = document.querySelector('#topBar');
+  let menuButton = document.querySelector('.menu-button');
+  let menuTexts = document.querySelector('.menu-texts');
+  let rwdTopButton = document.querySelector('#rwd-top-button');
+  let span = [...document.querySelectorAll('#rwd-top-button span')]
+  let spanDiv = [...document.querySelectorAll('#rwd-top-button span div')]
+  let rwdBlack = document.querySelector('#rwdblack');
+  
+  
+  
+  
+  function menuButtonHLer(e) {    
+    
+    let {currentTarget} = e
+    span[0].style.transform = "translate(0)";
+    span[2].style.transform = "translate(0)";
+    span[1].style.transform = "scaleX(0)";
+    spanDiv[0].style.transform = "rotateZ(45deg) scaleX(1)";
+    spanDiv[2].style.transform = "rotateZ(-45deg) scaleX(1)";
+    rwdTopButton.style.transform = "scale(1)";
+    menuTexts.classList.add('menuPop');
+    rwdBlack.classList.add('blackPop');
+    
+    
+  }
+  function topButtonHLer() {
+    span[0].style.transform = "translate(-27%,13%)";
+    span[2].style.transform = "translate(-27%,-13%)";
+    span[1].style.transform = "scaleX(1)";
+    spanDiv[0].style.transform = "rotateZ(45deg) scaleX(0.5)";
+    spanDiv[2].style.transform = "rotateZ(-45deg) scaleX(0.5)";
+    rwdTopButton.style.transform = "scale(0.7)";
+    menuTexts.classList.remove('menuPop');
+    rwdBlack.classList.remove('blackPop');
+    
+    
+   
+  }
+
   window.addEventListener('scroll',menuOffset)
+  menuButton.addEventListener('click',menuButtonHLer)
+  rwdTopButton.addEventListener('click',topButtonHLer)
+  rwdBlack.addEventListener('click',topButtonHLer)
+ 
+  
   
   
   
--- preDom.js
+++ preDom.js
@@ -1,138 +1,81 @@
-{
+(async () => {
     //開始
+  
+ 
+  //pre開始
+  
+//   fetch('header.html')
+//   .then(function(response) {
+//   // When the page is loaded convert it to text
+//   return response.text()
+//   })
+//   .then(function(html) {
+  
+//   document.querySelector('header').innerHTML = html;
+  
+  
+//   })
+//   .catch(function(err) {  
+//   console.log('Failed to fetch page: ', err);  
+//   })
+  
+  
+//   fetch('footer.html')
+//   .then(function(response) {
+//   // When the page is loaded convert it to text
+//   return response.text()
+//   })
+//   .then(function(html) {
+  
+//   document.querySelector('footer').innerHTML = html;
+  
+//   })
+//   .catch(function(err) {  
+//   console.log('Failed to fetch page: ', err);  
+//   });
+  
+axios.get('header.html')
+.then((res) => {
+    this.markDownData = res.data
+    document.querySelector('header').innerHTML = res.data;    
+}).catch((error) => {
+    console.error(error) 
+}).then(()=>{
+    const jsScript = document.createElement('script')
+    jsScript.src ='always.js'
+    document.body.append(jsScript)
+})
 
-fetch('header.html')
-    .then(function(response) {
-        // When the page is loaded convert it to text
-        return response.text()
-    })
-    .then(function(html) {
-        
-        document.querySelector('header').innerHTML = html;
-       
-    })
-    .catch(function(err) {  
-        console.log('Failed to fetch page: ', err);  
-    });
+axios.get('footer.html')
+.then((res) => {
+    this.markDownData = res.data
+    document.querySelector('footer').innerHTML = res.data;    
+}).catch((error) => {
+    console.error(error) 
+})
 
-    fetch('footer.html')
-    .then(function(response) {
-        // When the page is loaded convert it to text
-        return response.text()
-    })
-    .then(function(html) {
-        
-        document.querySelector('footer').innerHTML = html;
-       
-    })
-    .catch(function(err) {  
-        console.log('Failed to fetch page: ', err);  
-    });
-    (async () => {
-        //開始
-      
-      function preDom() {
-      //pre開始
-      let header = document.querySelector('header');
-      let menu = document.querySelector('#menu'); 
-      fetch('header.html')
-      .then(function(response) {
-      // When the page is loaded convert it to text
-      return response.text()
-      })
-      .then(function(html) {
-      
-      document.querySelector('header').innerHTML = html;
-      
-      })
-      .catch(function(err) {  
-      console.log('Failed to fetch page: ', err);  
-      })
-      
-      
-      fetch('footer.html')
-      .then(function(response) {
-      // When the page is loaded convert it to text
-      return response.text()
-      })
-      .then(function(html) {
-      
-      document.querySelector('footer').innerHTML = html;
-      
-      })
-      .catch(function(err) {  
-      console.log('Failed to fetch page: ', err);  
-      });
-      
-      function innerHTML() {  
-        
-        
-      
-      let reportWindowSize = ()=> {  
-        return  
-                                
-        header.style.marginTop = window.innerHeight*20/100+'px';
-        }
-      
-      reportWindowSize()
-      window.addEventListener('resize', reportWindowSize);
-      
-      
-      
-      }
-      
-      //predom end
-      }
-      preDom()
-      
-      })().then(()=>{
-      document.addEventListener("DOMContentLoaded",()=>{
-      
-      const jsScript = document.createElement('script')
-      jsScript.src ='always.js'
-      document.body.append(jsScript)
-      
-      
-      });
-      
-      
-      
-      //結束
-      })
-//     fetch("image/social-color-1_logo-facebook.svg")
-//   .then(response => response.text())
-//   .then(svg => document.querySelector('#facebook_Icon').insertAdjacentHTML("afterbegin", svg));
 
-//   fetch("image/social-color-1_logo-youtube.svg")
-//   .then(response => response.text())
-//   .then(svg => document.querySelector('#youtube_Icon').insertAdjacentHTML("afterbegin", svg));
-
+  
+  //predom end
+  
+ 
+  
+  
+  })().then(()=>{
+      
+   
+   
     
-
-    function innerHTML() {  
-        
-        
-
-        let reportWindowSize = ()=> {  
-            return  
-                                    
-            header.style.marginTop = window.innerHeight*20/100+'px';
-            }
-
-        reportWindowSize()
-        window.addEventListener('resize', reportWindowSize);
-        
-        
-        
-    }
-    
-    
-    
-    
-
-document.addEventListener("DOMContentLoaded",innerHTML);
-let header = document.querySelector('header');
-        let menu = document.querySelector('#menu');
-
+//   document.addEventListener("DOMContentLoaded",()=>{
+  
+//   const jsScript = document.createElement('script')
+//   jsScript.src ='always.js'
+//   document.body.append(jsScript)
+  
+  
+//   });
+  
+  
+  
   //結束
-}
+  })
